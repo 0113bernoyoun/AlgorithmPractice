c9be0643e6d682d48db9b62ef6edd497f6e9ef3b
bernoyoun <35679738+0113bernoyoun@users.noreply.github.com>
2023-06-12 16:23:15 +0900

    [level 0] Title: 세균 증식, Time: 0.03 ms, Memory: 73 MB -BaekjoonHub

---
+++ 프로그래머스/lv0/120910. 세균 증식/세균 증식.java
@@ -0,0 +1,11 @@
+class Solution {
+    public int solution(int n, int t) {
+        int i = 0;
+        int answer = n;
+        while(i < t){
+            answer *= 2;
+            i++;
+        }
+        return answer;
+    }
+}
