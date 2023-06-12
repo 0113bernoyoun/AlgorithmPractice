ba86f3de9a6d38ce8d56ee70cdab5fd5ad7b99e3
bernoyoun <35679738+0113bernoyoun@users.noreply.github.com>
2023-06-12 16:58:41 +0900

    [level 0] Title: 주사위의 개수, Time: 0.01 ms, Memory: 72.3 MB -BaekjoonHub

---
+++ 프로그래머스/lv0/120845. 주사위의 개수/주사위의 개수.java
@@ -0,0 +1,12 @@
+class Solution {
+    public int solution(int[] box, int n) {
+        int answer = 0;
+        int width = box[0];
+        int depth = box[1];
+        int height = box[2];
+        
+
+        
+        return (width / n) * (depth / n) * (height / n);
+    }
+}
